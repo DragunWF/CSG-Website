7049ea4b3350c3e67b9bba0ec850dfd88c04f362
DragunDev <plarisanmarc@gmail.com>
2025-02-09 15:14:30 +0800

    Split code into multiple components and add dynamic additions of members

--- src/App.js
+++ src/App.js
@@ -1,295 +1,148 @@
+import { members } from "./helpers/members";
+
 function App() {
   return (
-    <div class="wrapper">
+    <div class="app">
       <div class="home">
-        <div class="overlap">
-          <img
-            class="gradient"
-            src="https://c.animaapp.com/Bfeg6Lq5/img/gradient.png"
-            alt="gradient"
-          />
-          <div class="rectangle"></div>
-          <div class="text-wrapper">CSG Officers and Representatives</div>
-        </div>
-        <div class="overlap-group">
-          <div class="div">
-            <div class="rectangle-2"></div>
-            <img
-              class="IMG"
-              src="https://c.animaapp.com/Bfeg6Lq5/img/img-7623-1.png"
-              alt="gradient"
-            />
-            <img
-              class="img"
-              src="https://c.animaapp.com/Bfeg6Lq5/img/gradient-1.png"
-              alt="gradient"
-            />
-            <div class="rectangle-3"></div>
-            <p class="p">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </p>
-            <div class="NEWS-UPDATES">NEWS &amp; UPDATES</div>
-            <div class="rectangle-4"></div>
-            <img
-              class="intersect"
-              src="https://c.animaapp.com/Bfeg6Lq5/img/intersect.png"
-              alt="intersect"
-            />
-            <div class="text-wrapper-2">Home</div>
-            <div class="text-wrapper-2">Home</div>
-            <img
-              class="IMG-2"
-              src="https://c.animaapp.com/Bfeg6Lq5/img/img-4577-1@2x.png"
-              alt="cover"
-            />
-            <div class="text-wrapper-3">Member</div>
-            <div class="text-wrapper-4">Resources</div>
-            <div class="rectangle-5"></div>
-            <img class="ellipse" src="img/ellipse-2.svg" alt="rectangle" />
-            <img
-              class="group"
-              src="https://c.animaapp.com/Bfeg6Lq5/img/group-3@2x.png"
-              alt="rectangle"
-            />
-            <img
-              class="rectangle-6"
-              src="img/rectangle-13.svg"
-              alt="rectangle"
-            />
-            <div class="MISSION-VISION">MISSION &amp; VISION</div>
-            <div class="college-student">
-              College <br />
-              Student
-              <br />
-              Government
-            </div>
-            <p class="text-wrapper-5">By the students, For the students.</p>
-            <img
-              class="img-2"
-              src="https://c.animaapp.com/Bfeg6Lq5/img/home.svg"
-              alt="rectangle"
-            />
-            <img
-              class="rectangle-7"
-              src="img/rectangle-7.svg"
-              alt="rectangle"
-            />
-            <img
-              class="search"
-              src="https://c.animaapp.com/Bfeg6Lq5/img/search.svg"
-              alt="rectangle"
-            />
-            <img
-              class="rectangle-8"
-              src="img/rectangle-8.svg"
-              alt="rectangle"
-            />
-            <div class="rectangle-9"></div>
-            <div class="rectangle-10"></div>
-            <p class="text-wrapper-6">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </p>
-            <div class="text-wrapper-7">CONTACTS</div>
-            <div class="rectangle-11"></div>
-            <div class="text-wrapper-8">CLICK LINK</div>
-          </div>
-          <div class="div-wrapper">
-            <div class="text-wrapper-9">Explore</div>
-          </div>
-        </div>
-        <img class="rectangle-12" src="img/rectangle-1.svg" alt="rectangle" />
-        <div class="text-wrapper-10">Student Officials</div>
-        <div class="text-wrapper-11">Meet our dedicated</div>
-        <img class="rectangle-13" src="img/rectangle-19.svg" alt="rectangle" />
-        <div class="frame">
-          <div class="overlap-group-2">
-            <div class="overlap-2">
-              <div class="rectangle-14"></div>
-              <div class="text-wrapper-12">Position</div>
-              <div class="frame-2"></div>
-              <div class="text-wrapper-13">Surname, First Name M.I</div>
-              <img
-                class="chevron-down"
-                src="https://c.animaapp.com/Bfeg6Lq5/img/chevron-down-14.svg"
-                alt="chevron"
-              />
-            </div>
-            <div class="overlap-3">
-              <div class="rectangle-15"></div>
-              <div class="frame-2"></div>
-              <div class="text-wrapper-14">Position</div>
-              <div class="text-wrapper-15">Surname, First Name M.I</div>
-              <img
-                class="chevron-down-2"
-                src="https://c.animaapp.com/Bfeg6Lq5/img/chevron-down-14.svg"
-                alt="chevron"
-              />
-            </div>
-            <div class="overlap-4">
-              <div class="frame-3"></div>
-              <div class="overlap-5">
-                <div class="text-wrapper-16">Position</div>
-                <div class="text-wrapper-17">Surname, First Name M.I</div>
-                <img
-                  class="chevron-down-3"
-                  src="https://c.animaapp.com/Bfeg6Lq5/img/chevron-down-14.svg"
-                  alt="chevron"
-                />
-              </div>
-            </div>
-            <div class="overlap-6">
-              <div class="rectangle-16"></div>
-              <div class="frame-2"></div>
-              <div class="text-wrapper-18">Position</div>
-              <div class="text-wrapper-19">Surname, First Name M.I</div>
-              <img
-                class="chevron-down-4"
-                src="https://c.animaapp.com/Bfeg6Lq5/img/chevron-down-14.svg"
-                alt="chevron"
-              />
-            </div>
-            <div class="overlap-7">
-              <div class="rectangle-14"></div>
-              <div class="frame-2"></div>
-              <div class="text-wrapper-20">Position</div>
-              <div class="text-wrapper-21">Surname, First Name M.I</div>
-              <img
-                class="chevron-down-5"
-                src="https://c.animaapp.com/Bfeg6Lq5/img/chevron-down-14.svg"
-                alt="chevron"
-              />
-            </div>
-            <div class="overlap-8">
-              <div class="rectangle-14"></div>
-              <div class="frame-2"></div>
-              <div class="text-wrapper-22">Position</div>
-              <div class="text-wrapper-23">Surname, First Name M.I</div>
-              <img
-                class="chevron-down-6"
-                src="https://c.animaapp.com/Bfeg6Lq5/img/chevron-down-14.svg"
-                alt="chevron"
-              />
-            </div>
-            <div class="overlap-9">
-              <div class="rectangle-15"></div>
-              <div class="frame-2"></div>
-              <div class="text-wrapper-24">Position</div>
-              <div class="text-wrapper-25">Surname, First Name M.I</div>
-              <img
-                class="chevron-down-7"
-                src="https://c.animaapp.com/Bfeg6Lq5/img/chevron-down-14.svg"
-                alt="chevron"
-              />
-            </div>
-            <div class="overlap-10">
-              <div class="rectangle-17"></div>
-              <div class="frame-2"></div>
-              <div class="text-wrapper-26">Position</div>
-              <div class="text-wrapper-27">Surname, First Name M.I</div>
-              <img
-                class="chevron-down-7"
-                src="https://c.animaapp.com/Bfeg6Lq5/img/chevron-down-15.svg"
-                alt="chevron"
-              />
-            </div>
-            <div class="overlap-11">
-              <img
-                class="chevron-down-8"
-                src="https://c.animaapp.com/Bfeg6Lq5/img/chevron-down-15.svg"
-                alt="chevron"
-              />
-              <img
-                class="rectangle-18"
-                src="https://c.animaapp.com/Bfeg6Lq5/img/rectangle-40.svg"
-                alt="chevron"
-              />
-              <div class="text-wrapper-28">Position</div>
-              <div class="text-wrapper-29">Surname, First Name M.I</div>
-              <div class="frame-4"></div>
-              <img
-                class="chevron-down-9"
-                src="https://c.animaapp.com/Bfeg6Lq5/img/chevron-down-15.svg"
-                alt="chevron"
-              />
-            </div>
-            <div class="overlap-12">
-              <div class="rectangle-19"></div>
-              <div class="text-wrapper-12">Position</div>
-              <div class="frame-2"></div>
-              <div class="text-wrapper-13">Surname, First Name M.I</div>
-              <img
-                class="chevron-down"
-                src="https://c.animaapp.com/Bfeg6Lq5/img/chevron-down-15.svg"
-                alt="chevron"
-              />
-            </div>
-            <div class="overlap-13">
-              <div class="rectangle-15"></div>
-              <div class="frame-2"></div>
-              <div class="text-wrapper-14">Position</div>
-              <div class="text-wrapper-15">Surname, First Name M.I</div>
-              <img
-                class="chevron-down-2"
-                src="https://c.animaapp.com/Bfeg6Lq5/img/chevron-down-15.svg"
-                alt="chevron"
-              />
-            </div>
-            <div class="overlap-14">
-              <div class="frame-3"></div>
-              <div class="overlap-5">
-                <div class="text-wrapper-16">Position</div>
-                <div class="text-wrapper-17">Surname, First Name M.I</div>
-                <img
-                  class="chevron-down-3"
-                  src="https://c.animaapp.com/Bfeg6Lq5/img/chevron-down-15.svg"
-                  alt="chevron"
-                />
-              </div>
-            </div>
-            <div class="overlap-15">
-              <div class="rectangle-16"></div>
-              <div class="frame-2"></div>
-              <div class="text-wrapper-18">Position</div>
-              <div class="text-wrapper-19">Surname, First Name M.I</div>
-              <img
-                class="chevron-down-4"
-                src="https://c.animaapp.com/Bfeg6Lq5/img/chevron-down-15.svg"
-                alt="chevron"
-              />
-            </div>
-            <div class="overlap-16">
-              <div class="rectangle-14"></div>
-              <img
-                class="chevron-down-10"
-                src="https://c.animaapp.com/Bfeg6Lq5/img/chevron-down-15.svg"
-                alt="chevron"
-              />
-              <div class="frame-2"></div>
-              <div class="text-wrapper-20">Position</div>
-              <div class="text-wrapper-21">Surname, First Name M.I</div>
-            </div>
-            <div class="overlap-17">
-              <div class="rectangle-15"></div>
-              <div class="frame-2"></div>
-              <div class="text-wrapper-24">Position</div>
-              <div class="text-wrapper-25">Surname, First Name M.I</div>
-              <img
-                class="chevron-down"
-                src="https://c.animaapp.com/Bfeg6Lq5/img/chevron-down-14.svg"
-                alt="chevron"
-              />
-            </div>
-          </div>
-        </div>
-        <img
-          class="intersect-2"
-          src="https://c.animaapp.com/Bfeg6Lq5/img/intersect-1.png"
-          alt="intersect"
-        />
+        <HomeSection />
+        <MemberSection />
+        <Footer />
       </div>
     </div>
+  );
+}
+
+function HomeSection() {
+  return (
+    <div class="overlap-group">
+      <div class="div">
+        <div class="rectangle-2"></div>
+        <img
+          class="IMG"
+          src="https://c.animaapp.com/Bfeg6Lq5/img/img-7623-1.png"
+          alt="gradient"
+        />
+        <img
+          class="img"
+          src="https://c.animaapp.com/Bfeg6Lq5/img/gradient-1.png"
+          alt="gradient"
+        />
+        <div class="rectangle-3"></div>
+        <p class="p">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+        <div class="NEWS-UPDATES">NEWS &amp; UPDATES</div>
+
+        <div class="rectangle-4"></div>
+        <img
+          class="intersect"
+          src="https://c.animaapp.com/Bfeg6Lq5/img/intersect.png"
+          alt="intersect"
+        />
+        <div class="rectangle-5"></div>
+        <div class="text-wrapper-2">Home</div>
+        <img
+          class="IMG-2"
+          src="https://c.animaapp.com/Bfeg6Lq5/img/img-4577-1@2x.png"
+          alt="cover"
+        />
+        <div class="text-wrapper-3">Member</div>
+        <div class="text-wrapper-4">Resources</div>
+        <img class="ellipse" src="img/ellipse-2.svg" alt="rectangle" />
+        <img
+          class="group"
+          src="https://c.animaapp.com/Bfeg6Lq5/img/group-3@2x.png"
+          alt="rectangle"
+        />
+        <img class="rectangle-6" src="img/rectangle-13.svg" alt="rectangle" />
+        <div class="MISSION-VISION">MISSION &amp; VISION</div>
+        <div class="college-student">
+          College <br />
+          Student
+          <br />
+          Government
+        </div>
+        <p class="text-wrapper-5">By the students, For the students.</p>
+        <img
+          class="img-2"
+          src="https://c.animaapp.com/Bfeg6Lq5/img/home.svg"
+          alt="rectangle"
+        />
+        <img class="rectangle-7" src="img/rectangle-7.svg" alt="rectangle" />
+        <img
+          class="search"
+          src="https://c.animaapp.com/Bfeg6Lq5/img/search.svg"
+          alt="rectangle"
+        />
+        <img class="rectangle-8" src="img/rectangle-8.svg" alt="rectangle" />
+        <div class="rectangle-9"></div>
+        <div class="rectangle-10"></div>
+        <p class="text-wrapper-6">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+        <div class="text-wrapper-7">CONTACTS</div>
+        <div class="rectangle-11"></div>
+        <div class="text-wrapper-8">CLICK LINK</div>
+      </div>
+      <div class="div-wrapper">
+        <div class="text-wrapper-9">Explore</div>
+      </div>
+    </div>
+  );
+}
+
+function MemberSection() {
+  return (
+    <div>
+      <img class="rectangle-12" src="img/rectangle-1.svg" alt="rectangle" />
+      <div class="text-wrapper-10">Student Officials</div>
+      <div class="text-wrapper-11">Meet our dedicated</div>
+      <div class="overlap">
+        <img
+          class="gradient"
+          src="https://c.animaapp.com/Bfeg6Lq5/img/gradient.png"
+          alt="gradient"
+        />
+        <div class="rectangle"></div>
+        <div class="text-wrapper">CSG Officers 2024-2025</div>
+      </div>
+      <img class="rectangle-13" src="img/rectangle-19.svg" alt="rectangle" />
+      <div class="frame">
+        <div class="overlap-group-2 members">
+          {members.map((member) => (
+            <MemberCard name={member.name} position={member.position} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function MemberCard({ name, position }) {
+  return (
+    <div class="overlap-2">
+      <div class="rectangle-14"></div>
+      <div class="text-wrapper-12">{position}</div>
+      <div class="frame-2"></div>
+      <div class="text-wrapper-13">{name}</div>
+      <img
+        class="chevron-down"
+        src="https://c.animaapp.com/Bfeg6Lq5/img/chevron-down-14.svg"
+        alt="chevron"
+      />
+    </div>
+  );
+}
+
+function Footer() {
+  return (
+    <img
+      class="intersect-2"
+      src="https://c.animaapp.com/Bfeg6Lq5/img/intersect-1.png"
+      alt="intersect"
+    />
   );
 }
 
---
+++ src/helpers/members.js
@@ -0,0 +1,58 @@
+export const members = [
+  {
+    name: "Marc Plarisan",
+    position: "IT Representative | Web Developer",
+  },
+  {
+    name: "Rein Villanueva",
+    position: "President",
+  },
+  {
+    name: "Jayvee Liwanag",
+    position: "Internal Vice President",
+  },
+  {
+    name: "Ehl Kurt",
+    position: "External Vice President",
+  },
+  {
+    name: "Symon Villamor",
+    position: "Secretary",
+  },
+  {
+    name: "Gabby Cubelo",
+    position: "Treasurer",
+  },
+  {
+    name: "Angel Dugan",
+    position: "Auditor",
+  },
+  {
+    name: "Nathan Villamor",
+    position: "Computer Engineering Representative",
+  },
+  {
+    name: "Andrea Gurtiza",
+    position: "Accountancy Representative",
+  },
+  {
+    name: "Clarrisa Arnaiz",
+    position: "Arts and Sciences Representative",
+  },
+  {
+    name: "Justine Bangcal",
+    position: "Criminology Representative",
+  },
+  {
+    name: "Jhonas Lalisa Peralta",
+    position: "Hospitality Management Representative",
+  },
+  {
+    name: "Fred Fajardo",
+    position: "IT Representative",
+  },
+  {
+    name: "Trisha Denice Narciso",
+    position: "Tourism Management Representative",
+  },
+];
